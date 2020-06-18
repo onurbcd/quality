94fd4e6ae2fcea7ff84edfe3ae54e24d63a56d52
Bruno Domingues <onurbcd@gmail.com>
2020-06-18 12:36:14 -0300

    Add thread to run analysis

--- com.onurbcd.qa/src/com/onurbcd/qa/handlers/AnalysisHandler.java
+++ com.onurbcd.qa/src/com/onurbcd/qa/handlers/AnalysisHandler.java
@@ -3,23 +3,14 @@ package com.onurbcd.qa.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
 
-import com.onurbcd.qa.manager.AnalysisManager;
-
-import org.eclipse.jface.dialogs.MessageDialog;
+import com.onurbcd.qa.manager.JobManager;
 
 public class AnalysisHandler extends AbstractHandler {
 
-	private AnalysisManager analysisManager = new AnalysisManager();
-
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		analysisManager.run();
-		String message = analysisManager.getMessage();
-        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-        MessageDialog.openInformation(window.getShell(), "Quality", message);
+		JobManager.scheduleJob(event);
         return null;
 	}
 }
---
+++ com.onurbcd.qa/src/com/onurbcd/qa/manager/JobManager.java
@@ -0,0 +1,42 @@
+package com.onurbcd.qa.manager;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class JobManager {
+
+	private JobManager() {
+	}
+
+	private static final AnalysisManager ANALYSIS_MANAGER = new AnalysisManager();
+
+	public static void scheduleJob(ExecutionEvent event) {
+		Job job = Job.create("Analyse method", monitor -> {
+			run();
+			syncWithUi(event);
+		});
+
+		job.schedule();
+	}
+
+	private static void run() {
+		ANALYSIS_MANAGER.run();
+	}
+
+	private static void syncWithUi(ExecutionEvent event) {
+		Display.getDefault().asyncExec(() -> {
+			try {
+				IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+				String message = ANALYSIS_MANAGER.getMessage();
+				MessageDialog.openInformation(window.getShell(), "Quality", message);
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+}
